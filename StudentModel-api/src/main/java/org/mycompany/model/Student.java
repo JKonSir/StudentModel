b9aa2355683b7845ea9e725724b4d78499972fa7
KonstantinAMP <gijoe403@gmail.com>
2017-05-30 22:08:05 +0400

    -- add hibernate-enhance-maven-plugin to solve the problem with lazy load students for group -- set default fetch type of Group parameter for Student entity

--- StudentModel-api/src/main/java/org/mycompany/model/Student.java
+++ StudentModel-api/src/main/java/org/mycompany/model/Student.java
@@ -7,7 +7,6 @@ import java.math.BigInteger;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -48,7 +47,7 @@ public class Student implements GenericEntity, Serializable
 
     @Setter(value = AccessLevel.NONE)
     @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "group_number",
             referencedColumnName = "group_number",
             insertable = false,
